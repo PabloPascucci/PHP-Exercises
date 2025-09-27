7dd0fdb90a8a6d6db2de0a914ecc230c4784f137
PabloPascucci <pascuccipablo20@gmail.com>
2025-09-26 23:49:00 -0300

    Create Repo PHP exercise

---
+++ Ejercicios-logicos/fizzbuzz.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fizz Buzz</title>
+</head>
+<body>
+
+<div>
+    <h1>#1 El famoso "Fizz buzz"</h1>
+    <p>Ejercicio 1 de <a href="https://retosdeprogramacion.com/ejercicios/">https://retosdeprogramacion.com/ejercicios/</a></p>
+    <h3>Escribe un programa que muestre los números de 1 a 100, sustituyendo los siguientes:</h3>
+    <ul>
+        <li>Múltiplos de 3 por la palabra "fizz".</li>
+        <li>Múltiplos de 5 por la palabra "buzz".</li>
+        <li>Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".</li>
+    </ul>
+</div>
+    
+</body>
+</html>
